6a63609ea055e285b370aef025aa951344ba366b
Dave Frey <perfectritone@gmail.com>
2018-11-25 23:05:48 -0500

    Setting up db

--- config/dev.exs
+++ config/dev.exs
@@ -68,8 +68,9 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :find_your_setlist, FindYourSetlist.Repo,
-  username: "postgres",
-  password: "postgres",
+  adapter: Ecto.Adapters.Postgres,
+  username: "muunbeam",
+  password: "",
   database: "find_your_setlist_dev",
   hostname: "localhost",
   pool_size: 10
