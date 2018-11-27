80f80e3beb826cd4844310a67edaaae63d141105
Dave Frey <perfectritone@gmail.com>
2018-11-26 23:42:52 -0500

    WIP Adding tests

--- lib/find_your_setlist/bandcamp_scraper.ex
+++ lib/find_your_setlist/bandcamp_scraper.ex
@@ -1,6 +1,14 @@
 defmodule BandcampScraper do
   @bandcamp_domain "https://music.papadosio.com"
 
+  def scrape_sets do
+    releases_url()
+    |> page_body
+    # |> extract_sets
+    # |> Enum.map(&BandcampSetParser.parse(&1))
+    # |> Enum.map(&persist_set(&1))
+  end
+
   def releases_url do
     @bandcamp_domain
     |> URI.parse
@@ -17,4 +25,33 @@ defmodule BandcampScraper do
     |> Floki.attribute("data-initial-values")
     |> Enum.map(&Jason.decode(&1))
   end
+
+  # def persist_set(set) do
+  #   # Persist set
+  #   page_body(releases_url ++ set.urn)
+  #   |> extract_set_songs
+  # end
+
+  # def extract_set_songs(html) do
+  #   Floki.find(html, "td.title-col .title")
+  #   |> extract_song
+  # end
+
+  # def extract_song(song_html) do
+  #   %{
+  #     urn: Floki.attribute(song_html, "a") |> Floki.attribute("href"),
+  #     name: Floki.attribute(song_html, "a span") |> Floki.text,
+  #     duration: Floki.attribute(song_html, ".time") |> Floki.text |> extract_song_duration,
+  #   }
+  # end
+
+  # def extract_song_duration(raw_duration) do
+  #   String.split(raw_duration, ":")
+  #   # |>
+  #   #   reverse.each_with_index.reduce(0) do |sum, (value, index)|
+  #   #     sum + (value.to_i * 60 ** index)
+  #   #   end
+  # end
+
+  # #  mix phx.gen.schema Performance performances release_date:date raw_title:string bandcampbcbcbc_id:string urn:string
 end
---
+++ lib/find_your_setlist/bandcamp_set_parser.ex
@@ -0,0 +1,11 @@
+defmodule BandcampSetParser do
+  def parse({:ok, raw_set}) do
+    %{
+      # date: ~D[raw_set.release_date],
+      date: raw_set.release_date,
+      title: raw_set.title,
+      bandcamp_id: raw_set.id,
+      urn: raw_set.page_url,
+    }
+  end
+end
